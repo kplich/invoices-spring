06620518ed13a09c014d358af1c01ec4fb39e908
Kamil Plich <kamilplich2@gmail.com>
2018-08-04 11:20:47 +0200

    created model

---
+++ src/main/java/kplich/invoices/model/Invoice.java
@@ -0,0 +1,37 @@
+package kplich.invoices.model;
+
+import javax.persistence.*;
+import java.time.*;
+
+@Entity
+public class Invoice {
+	@Id
+	private String invoiceId;
+
+	private LocalDate issueDate;
+	private LocalDate saleDate;
+
+	public String getInvoiceId() {
+		return invoiceId;
+	}
+
+	public void setInvoiceId(String invoiceId) {
+		this.invoiceId = invoiceId;
+	}
+
+	public LocalDate getIssueDate() {
+		return issueDate;
+	}
+
+	public void setIssueDate(LocalDate issueDate) {
+		this.issueDate = issueDate;
+	}
+
+	public LocalDate getSaleDate() {
+		return saleDate;
+	}
+
+	public void setSaleDate(LocalDate saleDate) {
+		this.saleDate = saleDate;
+	}
+}
---
+++ src/main/java/kplich/invoices/model/Order.java
@@ -0,0 +1,46 @@
+package kplich.invoices.model;
+
+import javax.persistence.*;
+import java.math.*;
+
+@Entity
+public class Order {
+	@Id
+	private int number;
+
+	private String loadingLocation;
+	private String unloadingLocation;
+	private BigDecimal value;
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+	public String getLoadingLocation() {
+		return loadingLocation;
+	}
+
+	public void setLoadingLocation(String loadingLocation) {
+		this.loadingLocation = loadingLocation;
+	}
+
+	public String getUnloadingLocation() {
+		return unloadingLocation;
+	}
+
+	public void setUnloadingLocation(String unloadingLocation) {
+		this.unloadingLocation = unloadingLocation;
+	}
+
+	public BigDecimal getValue() {
+		return value;
+	}
+
+	public void setValue(BigDecimal value) {
+		this.value = value;
+	}
+}
