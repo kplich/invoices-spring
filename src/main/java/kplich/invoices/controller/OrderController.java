eb812af373a4fe44dd440b9e39f5010946738f4c
Kamil Plich <kamilplich2@gmail.com>
2018-08-09 13:55:17 +0200

    adding and viewing orders

--- src/main/java/kplich/invoices/controller/OrderController.java
+++ src/main/java/kplich/invoices/controller/OrderController.java
@@ -2,12 +2,14 @@ package kplich.invoices.controller;
 
 import kplich.invoices.model.*;
 import kplich.invoices.service.*;
+import org.springframework.stereotype.*;
+import org.springframework.ui.*;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.*;
 import java.util.*;
 
-@RestController
+@Controller
 @RequestMapping(path = "/orders")
 public class OrderController {
 	private MainService service;
@@ -17,16 +19,20 @@ public class OrderController {
 	}
 
 	@GetMapping(path = "/get")
-	public Optional<TransportOrder> getByNumber(@RequestParam int number) {
-		return service.getOrderRepository().findById(number);
+	public String getByNumber(@RequestParam int number, Model model) {
+		Optional<TransportOrder> result = service.getOrderRepository().findById(number);
+		model.addAttribute("order", result.get());
+		return "viewOrder";
 	}
 
 	@GetMapping(path = "/get/all")
+	@ResponseBody
 	public Iterable<TransportOrder> getAll() {
 		return service.getOrderRepository().findAll();
 	}
 
 	@GetMapping(path = "/get/invoice")
+	@ResponseBody
 	public Iterable<TransportOrder> getByInvoice(@RequestParam String invoiceId) {
 		Optional<Invoice> invoice = service.getInvoiceRepository().findById(invoiceId);
 
@@ -38,27 +44,32 @@ public class OrderController {
 		}
 	}
 
-	@PostMapping(path = "/add")
-	public boolean addOrUpdate(@Valid @ModelAttribute TransportOrder order,
-							   @RequestParam(required = false, defaultValue = "") String invoiceId) {
-		boolean result = false;
+	@GetMapping(path = "/add")
+	public String addOrUpdate() {
+		return "addOrder";
+	}
 
+	@PostMapping(path = "/add")
+	public String addOrUpdate(@Valid @ModelAttribute TransportOrder order,
+						      @RequestParam(required = false, defaultValue = "") String invoiceId,
+							  Model model) {
 		try {
 			Optional<Invoice> invoice = service.getInvoiceRepository().findById(invoiceId);
 
 			invoice.ifPresent(order::setInvoice); //woah, nice! ;)
 
 			service.getOrderRepository().save(order);
-			result = true;
 		}
 		catch (Exception e) {
 			e.printStackTrace(); //TODO no to jak to logowac?
 		}
 
-		return result;
+		model.addAttribute("order", order);
+		return "viewOrder";
 	}
 
 	@DeleteMapping(path = "/delete")
+	@ResponseBody
 	public boolean delete(@RequestParam int number) {
 		boolean result = false;
 
