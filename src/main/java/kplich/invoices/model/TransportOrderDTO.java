84f0df80b103a57e4cad66caa470bf5229f67853
Kamil Plich <kamilplich2@gmail.com>
2018-08-29 17:00:52 +0200

    transport order DTO with tax value, updated invoice view

---
+++ src/main/java/kplich/invoices/model/TransportOrderDTO.java
@@ -0,0 +1,28 @@
+package kplich.invoices.model;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+
+public class TransportOrderDTO {
+
+    private static final BigDecimal TAX = new BigDecimal("0.23");
+
+    private TransportOrder order;
+    private BigDecimal taxValue;
+
+    public TransportOrderDTO(TransportOrder order) {
+        this.order = order;
+        this.taxValue = order.getValue().multiply(TAX).setScale(2, RoundingMode.HALF_UP);
+    }
+
+    public TransportOrder getOrder() {
+
+        return order;
+    }
+
+    public BigDecimal getTaxValue() {
+
+        return taxValue;
+    }
+}
