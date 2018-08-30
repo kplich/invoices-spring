bfa54c7070b91e585ae59edd2495b039e9556758
Kamil Plich <kamilplich2@gmail.com>
2018-08-30 19:38:39 +0200

    renaming "view invoice" to "print invoice"

--- src/main/java/kplich/invoices/controller/InvoiceController.java
+++ src/main/java/kplich/invoices/controller/InvoiceController.java
@@ -26,15 +26,15 @@ public class InvoiceController {
 		return "viewInvoices";
 	}
 
-	@GetMapping(path = "/get")
-	public String viewInvoice(@RequestParam String id, Model model) {
+	@GetMapping(path = "/print")
+	public String printInvoice(@RequestParam String id, Model model) {
 
 	    Invoice invoice = service.getInvoice(id);
 	    InvoiceOutputDTO outputDTO = new InvoiceOutputDTO(invoice, service.getOrderDTOsWithInvoice(invoice));
 
 		model.addAttribute("invoiceDTO", outputDTO);
 
-		return "viewInvoice";
+		return "printInvoice";
 	}
 
 	@PostMapping(path = "/add")
